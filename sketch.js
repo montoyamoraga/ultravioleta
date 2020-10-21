3c070370d2faa86ba508b5b729e94c635a7a88a1
montoyamoraga <montoyamoraga@gmail.com>
2020-10-21 10:47:32 -0400

    add logic for new lines

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// nameTODO
+// casiVioleta
 // by Nicole L'Huillier + Aarón Montoya-Moraga
 // submission to Generative Unfoldings
 // October 2020
@@ -10,15 +10,28 @@ const oneFrameStartIndex = oneFrameText.length;
 
 let isOneFrame = null;
 
-const rnn = new ml5.charRNN("./models/input", modelLoaded);
+let rnn;
+
+let currentDecimas = null;
+
+let generating = false;
+
+let temperature = 0.5;
+
+let decimasLines = 10;
+let currentLine = 0;
+let justDidNewLine = false;
 
 // when the model is loaded
 function modelLoaded() {
   console.log("model loaded!");
+  generate();
 }
 
 function setup() {
-  createCanvas(400, 400);
+  // createCanvas(windowWidth, windowHeight);
+
+  noCanvas();
   
   url = getURL();
 
@@ -28,17 +41,27 @@ function setup() {
   
   print("lastPartUrl: " + lastPartUrl);
 
-  rnn.generate({ seed: 'a',
-                length: 300,
-                temperature: 0.5
-              }, (err, results) => {
-    console.log(results);
-  });  
+  rnn = new ml5.charRNN("./models/input", modelLoaded);
+
+
+  textAlign(CENTER);
+
 }
 
 function draw() {
-  background(220);
-  ellipse(random(width), random(height), 10, 10);
+  // background(255);
+  // if (currentDecimas != null) {
+  //   text(currentDecimas.sample, windowWidth/2, windowHeight/4);
+  // }
+  
+  // rnn.generate({ seed: 'a',
+  //               length: 200,
+  //               temperature: 0.5
+  //             }, (err, results) => {
+  //   console.log(results);
+  //   currentDecimas = results;
+  // });
+
 }
 
 function detectOneFrame() {
@@ -49,4 +72,46 @@ function detectOneFrame() {
     isOneFrame = false;
     loop();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function generate() {
+  if (generating) {
+    generating = false;
+  } else {
+    generating = true;
+    loopRNN();
+  }
+}
+
+async function loopRNN() {
+  while (generating) {
+    await predict();
+  }
+}
+
+async function predict() {
+  let par = select("#result");
+  let next = await rnn.predict(temperature);
+  await rnn.feed(next.sample);
+  if (next.sample == "\r" || next.sample == "\n") {
+    if (!justDidNewLine) {
+      par.html(par.html() + "<br/>");
+      justDidNewLine = true;
+      currentLine = currentLine + 1;
+    }
+  } else {
+    par.html(par.html() + next.sample);
+    justDidNewLine = false;
+  }
+
+  if (currentLine > decimasLines - 1) {
+    par.html(par.html() + "<br/> <br/>");
+    justDidNewLine = false;
+    currentLine = 0;
+  }
+  
 }
