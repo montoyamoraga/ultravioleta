02748596ea729f3e133d1b46223fce952ba2bc05
montoyamoraga <montoyamoraga@gmail.com>
2020-12-02 00:19:29 -0500

    tsonami ready

--- tsonami/script.py
+++ tsonami/script.py
@@ -1,3 +1,7 @@
+# code by aaron montoya-moraga
+# commissioned by nicole l'huillier
+# for tsonami 2020, chile
+
 # instructions: 
 # cd to the folder tsonami
 # run python script.py number
@@ -5,15 +9,61 @@
 # 0.0 is replace no words, 1.0 is replace all words
 # 0.05 is replace 5% of words
 
+# import modules
+import sys
+import random
+
+# variable for probability of changing words
+# default value is 5 percent
+probability = 0.05
+
+# if the command line has a number argument
+# replace the probability value with it
+if (len(sys.argv) == 2):
+  probability = float(sys.argv[1])
+
 # load original.txt
+original = open("original.txt", "r").read()
 
 # load keywords.txt
+keywords = open("keywords.txt", "r").read()
 
-# retrieve argument from command line for output
+# create array, where every element is a line
+keywords = keywords.split("\r\n");
 
-# create output file
+# create variable for storing current word
+currentWord = ""
 
-# go th
+# create variable for storing new text
+newText = ""
 
+# go through every character in the original text
+for i in range(len(original)):
+  
+  # if the character is not a space
+  if original[i] != " ":
+    # append the character to the current word
+    currentWord = currentWord + original[i]
+  # if the character is a space
+  else:
+    # pick a random number between 0.0-1.0
+    randomNumber = random.random()
 
-print(2 + 3)
+    # if the number is smaller than probability
+    # replace the currentWord with a random one from keywords
+    if randomNumber < probability:
+      currentWord = random.choice(keywords)
+
+    # append the word to the new text variable
+    newText = newText + currentWord + " "
+
+    # reset the current word
+    currentWord = ""
+
+# open file output.txt to rewrite it
+output = open("output.txt", "w+")
+
+output.write(newText)
+
+# close file
+output.close()
