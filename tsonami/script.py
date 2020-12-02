44b4456f5b7d18b8aaa2d1eff97b85c12dc9b40a
montoyamoraga <montoyamoraga@gmail.com>
2020-12-01 19:51:21 -0500

    add graffitis version

--- tsonami/script.py
+++ tsonami/script.py
@@ -1,6 +1,9 @@
 # instructions: 
 # cd to the folder tsonami
-# run python script.py
+# run python script.py number
+# number goes from 0.0 to 1.0
+# 0.0 is replace no words, 1.0 is replace all words
+# 0.05 is replace 5% of words
 
 # load original.txt
 
@@ -9,6 +12,8 @@
 # retrieve argument from command line for output
 
 # create output file
+
+# go th
 
 
 print(2 + 3)
